4eab658ec6848dec57becdb93873f7464c74d497
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-05 03:47:06 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print(str(i).zfill(2), end="")
+        print("{}".format(str(i).zfill(2)))
     else:
-         print(str(i).zfill(2), end=", ")
-print()
+         print("{}".format(str(i).zfill(2)), end=", ")
