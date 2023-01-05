5bd7f8b7c74d85017c3cb5684f95a714175a437a
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2023-01-05 23:30:50 +0100

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        print(number % 10, end="")
-    else:
-        print((number * -1) % 10, end="")
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
