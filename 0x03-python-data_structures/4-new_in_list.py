28c799cd9b9519079c1276ff4ade019d86c4edeb
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2023-01-08 00:59:32 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
-def replace_in_list(my_list, idx, element):
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    else:
-        newS = my_list.copy()
-        newS[idx] = element
-        return newS
+    
+    newL = [x for x in my_list]
+    newL[idx] = element
+    return newL
