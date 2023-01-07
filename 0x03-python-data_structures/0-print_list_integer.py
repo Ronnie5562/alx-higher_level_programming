296a5020512e812acead2304e9b906b577574bfc
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2023-01-07 23:50:20 +0100

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for x in my_list:
-        print("{}".format(x))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
