b9a5e625ab89b2009a00424c07b261ca448b1e71
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-24 00:57:09 +0200

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
 
-    sum = 0
-    for num in range(len(sys.argv) - 1):
-        sum += int(sys.argv[num + 1])
-    print("{}".format(sum)) 
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
