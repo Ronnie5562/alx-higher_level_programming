ca45d0976b7fb7f0f4f1697957faa97bf91605bd
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 23:45:16 +0200

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
 if __name__ == "__main__":
     """Prints out the CLI arguments."""
     from sys import argv
 
-    if len(argv) == 1:
-        print("{} argument".format(len(argv)))
+    if len(argv) == 0:
+        print("0 arguments.")
+    elif len(argv) == 1:
+        print("1 argument:")
     else:
-       print("{} arguments".format(len(argv)))
+       print("{} arguments:".format(len(argv) - 1))
     if len(argv) != 0:
         for index, arg in enumerate(argv):
             print("{}: {}".format(index + 1, arg))
