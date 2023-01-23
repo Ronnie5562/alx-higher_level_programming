a282755871ae51d24224eb8e7505521cc2f91b38
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-24 01:00:21 +0200

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
+    """Prints the sum of all the CL arguments."""
     import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    sum = 0
+    for num in range(len(sys.argv) - 1):
+        sum += int(sys.argv[num + 1])
+    print("{}".format(sum))
