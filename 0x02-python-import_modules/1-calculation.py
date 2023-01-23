5972fad546ce532fafab6ece6de57295ceda9d94
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 22:59:36 +0200

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints out the result of so"""
+    from calculator_1 import add, div, mul, sub
+
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
