c75276750f84dd9a8590b90fa2869e8f1cd9d835
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2023-01-23 22:05:30 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Prints 1 + 2 = 3"""
     from add_0 import add
 
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
