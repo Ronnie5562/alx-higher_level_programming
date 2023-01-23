9a28e9ce55f37f6595dfa24b155809d2bebd0563
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-24 00:04:37 +0200

    fixed bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Prints out the CLI arguments."""
-    from sys import argv
 
-    index = len(argv) - 1
-    if index == 0:
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif index == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(index))
-    for i in range(index):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
