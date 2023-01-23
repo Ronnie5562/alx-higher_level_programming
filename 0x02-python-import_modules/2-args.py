5dd74d6098f2fb0af3de6a7247f28e470d3fcae1
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 23:48:58 +0200

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
        print("{} arguments:".format(len(argv)))
     if len(argv) != 0:
-        for index, arg in enumerate(argv):
-            print("{}: {}".format(index + 1, arg))
+        for index in argv:
+            print("{}: {}".format(index + 1, argv[index + 1]))
