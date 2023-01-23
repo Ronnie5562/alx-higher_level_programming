58975d775c1a5be8cff74a14e1c5282f968ce740
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 23:57:11 +0200

    fixed bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     """Prints out the CLI arguments."""
     from sys import argv
 
-    if len(argv) == 0:
+    x = len(argv) - 1
+    if x == 0:
         print("0 arguments.")
-    elif len(argv) == 1:
+    elif x == 1:
         print("1 argument:")
     else:
-       print("{} arguments:".format(len(argv)))
-    if len(argv) != 0:
-        for index in range(len(argv) - 1):
-            print("{}: {}".format(index + 1, argv[index + 1]))
+       print("{} arguments:".format(x))
+    for index in range(x):
+        print("{}: {}".format(index + 1, argv[index + 1]))
