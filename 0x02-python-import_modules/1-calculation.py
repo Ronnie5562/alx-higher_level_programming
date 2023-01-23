d2d7e13d00a63a4f7c74d341e428b126904f90ee
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 23:02:21 +0200

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """Prints out the result of so"""
+    """Prints out the result of some arithmetic operations"""
     from calculator_1 import add, div, mul, sub
 
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} + {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, mul(a, b)))
