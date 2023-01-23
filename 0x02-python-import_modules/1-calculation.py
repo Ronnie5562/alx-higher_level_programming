25460f3f52f0753186b334a9db75c594d4a9a043
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-23 23:08:11 +0200

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Prints out the result of some arithmetic operations"""
-    from calculator_1 import add, div, mul, sub
+    """Print the sum, difference, product, quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
