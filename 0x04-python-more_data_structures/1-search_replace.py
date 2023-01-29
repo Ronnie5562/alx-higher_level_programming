d2360b9b3b082bc6d4fc31fa74c6b1807df5523d
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-29 09:59:42 +0200

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list
+    new_list = my_list[:]
     for num in range(len(new_list)):
        if new_list[num] == search:
            new_list[num] = replace
