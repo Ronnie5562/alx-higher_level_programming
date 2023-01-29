3a3da9265e2c830dd4970f699e2360d37d16ff23
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-29 10:01:14 +0200

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,4 +5,4 @@ def search_replace(my_list, search, replace):
     for num in range(len(new_list)):
        if new_list[num] == search:
            new_list[num] = replace
-    return new_list
+    return (new_list)
