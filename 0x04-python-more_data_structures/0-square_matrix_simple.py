46ae6eabce426c7a6ea568c0490c50bcec2ca9a4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-29 09:21:43 +0200

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    list_2 = []
-    for x in matrix:
-        list_1 = []
-        for y in x:
-            list_1.append(y * y)
-        list_2.append(list_1)
-    return list_2
+    [list(map(lambda y: y * y, x)) for x in matrix]
