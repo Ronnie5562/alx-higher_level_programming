6565463932275b09393af6d91dc39de9a94e5d01
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-02 20:55:32 +0200

    Task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,2 +1,7 @@
-from variable_load_5 import a
-print(a)
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Prints the value of variable a imported from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
