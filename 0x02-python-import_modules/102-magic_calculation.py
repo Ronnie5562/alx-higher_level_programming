a97e948402453929cffee9e260305881aabc806a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-02 21:59:28 +0200

    Task 6

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    from calculator_1 import add, sub
 
     if a < b:
         c = add(a, b)
