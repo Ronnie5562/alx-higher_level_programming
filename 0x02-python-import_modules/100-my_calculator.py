c690566278de9cccb8ed51ec6127aa02862ff03f
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-02 21:49:59 +0200

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """a program that imports all functions from the file calculator_1.py and handles basic operations."""
-    import sys
+    """A program that handles basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
+    import sys
+
     if len(sys.argv) - 1 != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    else:
-        if sys.argv[1] == '+':
-            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {add(int(sys.argv[1]), int(sys.argv[3]))}')
-        elif sys.argv[1] == '-':
-            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {sub(int(sys.argv[1]), int(sys.argv[3]))}')
-        elif sys.argv[1] == '*':
-            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {mul(int(sys.argv[1]), int(sys.argv[3]))}')
-        elif sys.argv[1] == '/':
-            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {div(int(sys.argv[1]), int(sys.argv[3]))}')
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            sys.exit(1)
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
