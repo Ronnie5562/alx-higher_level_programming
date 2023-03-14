829ccaeb5746750d34fa8597af191392ed47cb76
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-14 20:12:08 +0200

    new commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Records are ordered by score (top first)
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
