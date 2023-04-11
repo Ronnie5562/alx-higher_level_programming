2690ce7711a6c6189c7b2adf991cec42c53c6a67
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-11 19:23:17 +0200

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const length = process.argv.length;
-if (length === 2) {
-    console.log('No argument');
-} else if (length === 3) {
-    console.log('Argument found');
-} else {
-    console.log('Arguments found');
-}
+console.log(process.argv.length === 2 ? 'No argument' : process.argv.length === 3 ? 'Argument found' : 'Arguments found');
