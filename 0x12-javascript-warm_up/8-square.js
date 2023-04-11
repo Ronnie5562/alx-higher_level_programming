cb16205c582a9924df939fa16d229a6a537060a6
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-11 23:57:44 +0200

    A new commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const number = Number(process.argv[2]);
 if (isNaN(number)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 1; i <= number; i++) {
-        let line = '';
-        for (let i = 1; i <= number; i++) {
-            line += 'X';
-        }
-        console.log(line);
+  for (let i = 0; i < number; i++) {
+    let line = '';
+    for (let i = 0; i < number; i++){
+      line += 'X';
     }
+    console.log(line);
+  }
 }
