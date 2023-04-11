5df8a3dc89e48ea3028b5fa8885b8d6d22d73633
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-11 20:59:52 +0200

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const texts = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < texts.length; i++) {
     console.log(texts[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-for (let i = 0; i < )
+const number = Math.floor(Number(process.argv[2]));
+if (isNaN(number)) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i < number; i++) {
+        console.log('C is fun');
+    }
+}
