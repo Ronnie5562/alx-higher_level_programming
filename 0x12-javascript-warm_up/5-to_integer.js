be0968aed62f31a8e9595ff61349bbc3632f094c
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-11 19:46:44 +0200

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(Math.floor(process.argv[2]) ? `${Math.floor(process.argv[2])}` : 'Not a number');
+console.log(Math.floor(process.argv[2]) ? `My number: ${Math.floor(process.argv[2])}` : 'Not a number');
