952824f0bf35ada3ad82a9d992a0a90e2b5f6cd1
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-11 20:10:15 +0200

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-    console.log(line);
+const texts = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i <= texts.length; i++) {
+    console.log(texts[i]);
 }
