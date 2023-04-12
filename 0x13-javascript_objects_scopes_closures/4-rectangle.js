d94d4b15a864093ef9429d7a89f83095b2757244
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-12 19:24:16 +0200

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,15 +4,11 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) [this.width, this.height] = [w, h];
   }
 
-  print = () => {
+  print () {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
   
-  double = () => {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
-  }
+  double = () => [this.width, this.height] = [this.width * 2, this.height * 2];
   
-  rotate = () => {
-    [this.width, this.height] = [this.height, this.width];
-  }
+  rotate = () => [this.width, this.height] = [this.height, this.width];
 };
