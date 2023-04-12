da859936a70de6a84076a1303656ba8706c6d65b
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-12 21:05:04 +0200

    Task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
+const newDict = {};
 for (let key in dict) {
   !newDict[dict[key]] ? newDict[dict[key]] = [key] : newDict[dict[key]].push(key);
 }
