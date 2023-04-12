afaed3f58d0ed39d0fc144609325cc196fc47bad
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-12 21:02:53 +0200

    Task 11

--- 0x13-javascript_objects_scopes_closures/101-data.js
+++ 0x13-javascript_objects_scopes_closures/101-data.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+//#!/usr/bin/node
 exports.dict = {
   89: 1,
   90: 2,
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,7 @@
-#!/usr/bin/node
+//#!/usr/bin/node
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (!newDict[dict[key]]) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+  !newDict[dict[key]] ? newDict[dict[key]] = [key] : newDict[dict[key]].push(key);
 }
 console.log(newDict);
