2e19af146730d0756aeec6d8100088348fcdb7bc
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-13 21:48:45 +0200

    Task 9

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 function factorial (n) {
-  let answer = !n || isNaN(n) ? 1 : n * factorial(n - 1);
-  return answer;
+  return !n || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 console.log(factorial(Number(process.argv[2])));
