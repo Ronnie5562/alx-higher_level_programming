7784af0d80ad4332f3e4844abab5d9b15c859529
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-13 22:03:40 +0200

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  process.argv.pop(Math.max(...process.argv));
-  console.log(process.argv.pop(Math.max(...process.argv)));
+  let list = process.argv;
+  list.pop(Math.max(...list));
+  console.log(list.pop(Math.max(...list)));
 }
