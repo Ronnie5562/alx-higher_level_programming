1cd585d9f3ebcb95427e1c1d03e829a5b990ada4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-16 23:37:31 +0200

    A new commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,9 +7,9 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(sys.argv[4]))
-    [print(city) for city in c.fetchall() if city[1] == sys.argv[4]]
-    c.close()
-    db.close()
+  db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", passwd=sys.argv[2], db=sys.argv[3])
+  c = db.cursor()
+  c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(sys.argv[4]))
+  [print(city) for city in c.fetchall() if city[1] == sys.argv[4]]
+  c.close()
+  db.close()
