7a6d9f1409597879d75f8756d2b7b850507df3e4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-17 14:52:45 +0200

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", passwd=sys.argv[2], db=sys.argv[3])
+  db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", 
+                       passwd=sys.argv[2], db=sys.argv[3])
   c = db.cursor()
   state = str(sys.argv[4])
   c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (state,))
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+write a script that takes in arguments and displays all values in the 
+states table of hbtn_0e_0_usa where name matches the argument. 
+But this time, write one that is safe from MySQL injections!
+Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password> \
+                        <database name> <state name searched>
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cur()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
