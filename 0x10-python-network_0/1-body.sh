13feb5174531eb7a18b5f7eabc0a25c4cb3a552f
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-27 21:21:18 +0200

    A new commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Write a Bash script that takes in a URL, sends a GET request 
+# to the URL, and displays the body of the response
 curl -sfL "$1" -X GET
