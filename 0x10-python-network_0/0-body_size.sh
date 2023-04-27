5749d0464e7780f1cd7d0cbe6bf1935c5de5bad0
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-27 21:34:33 +0200

    A new commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, 
-length=$(curl -sI "$1" | grep -i Content-Length | cut -d " " -f2)
-echo $length
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
