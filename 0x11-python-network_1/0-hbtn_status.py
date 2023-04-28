71d9955f2a63c39d286050783db7fb1e20b27d69
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-28 23:15:24 +0200

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """__summary__
-Write a Python script that fetches https://alx-intranet.hbtn.io/status
+- Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 if __name__ == '__main__':
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as request:
         data = request.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(data)))
-        print("\t- content: {}$".format(data))
-        print("\t- utf8 content: {}$".format(data.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
